da9e48cb6a1f803aaa5ce223f668782874ea7316
danghai2110 <ngo368032@gmail.com>
2024-06-24 19:00:10 +0700

    Add project files.

---
+++ bai 8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Chuong trinh tinh boi so";
+	int n, b;
+	cout << "Nhap so: ";
+	cin >> n;
+	for (int i = 1; i <=10; i++) {
+		cout << "  " << n * i;
+		
+	}
+	return 0;
+	
+
+	}
